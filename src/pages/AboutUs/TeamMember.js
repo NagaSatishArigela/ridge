3e6342247ce42bfb92bbb5d94abd0b48f48f0d26
NagaSatishArigela <nagasatisharigela@gmail.com>
2024-04-25 00:35:57 +0530

    about us changes

---
+++ src/pages/AboutUs/TeamMember.js
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import "./index.css";
+
+const TeamMemberCard = ({ member }) => {
+  const [expanded, setExpanded] = useState(false);
+
+  const toggleDescription = () => {
+    setExpanded(!expanded);
+  };
+
+  return (
+    <div className="teamMember">
+      <img src={member.image} alt={member.name} className="teamImage" />
+      <div className="teamDetails">
+        <h3
+          style={{
+            fontFamily: "sans-serif",
+            fontSize: "35px",
+            fontWeight: "900",
+            margin: "25px 0px",
+          }}
+        >
+          {member.name}
+        </h3>
+        <h4>{member.title}</h4>
+        <div className={`description ${expanded ? "expanded" : ""}`}>
+          <p style={{ textAlign: "justify" }}>{member.description}</p>
+        </div>
+        <button
+          onClick={toggleDescription}
+          style={{
+            color: "#DD9C37",
+            border: "none",
+            fontSize: "20px",
+            padding: "10px",
+          }}
+        >
+          {expanded ? "...Read Less" : "...Read More"}
+        </button>
+        <div className="socialLinks">
+          <a href={member.facebook}>
+            <i className="fa fa-facebook"></i>
+          </a>
+          <a href={member.linkedin}>
+            <i className="fa fa-linkedin"></i>
+          </a>
+          <a href={member.twitter}>
+            <i className="fa-brands fa-x-twitter"></i>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TeamMemberCard;
--- src/pages/AboutUs/index.js
+++ src/pages/AboutUs/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Helmet } from "react-helmet";
 import aboutUs from "../../assets/images/tranquil1.png";
 import surya from "../../assets/images/surya-ridge-homes.jpg";
@@ -11,66 +11,78 @@ import "../../components/Styles/styles.css";
 import WhatsAppLink from "../../components/WhatsappLink";
 import Header from "../../components/TopHeader";
 import "./index.css";
+import TeamMemberCard from "./TeamMember";
 
 const teamMembers = [
   {
     image: surya,
     name: "Surya Penmetsa",
     title: "MANAGING DIRECTOR",
-    description: "As managing director of the Ridge Group, He is deeply passionate about creating and delivering value for the people around him. His intense focus on value enables him to cut the noise and drive team toward results. Meetings are always short with Surya. His cross boarder and cross cultural experience is apparent in every conversation. Contrarian in thinking and quick on decision making enables team to execute business rooted on rationalality than axiomatic. Pleasure and challenging to work with at the same time. His emphasys on systems enable us to come up with longterm solutions to the challenges during execution. Growth hacker at core, everyday is a fresh start.",
+    description:
+      "As managing director of the Ridge Group, He is deeply passionate about creating and delivering value for the people around him. His intense focus on value enables him to cut the noise and drive team toward results. Meetings are always short with Surya. His cross boarder and cross cultural experience is apparent in every conversation. Contrarian in thinking and quick on decision making enables team to execute business rooted on rationalality than axiomatic. Pleasure and challenging to work with at the same time. His emphasys on systems enable us to come up with longterm solutions to the challenges during execution. Growth hacker at core, everyday is a fresh start.",
     facebook: "#",
     linkedin: "#",
-    twitter: "#"
+    twitter: "#",
   },
   {
     image: raju,
     name: "Srinivas Raju Vetukuri",
     title: "MANAGING PARTNER",
-    description: "Srinivas is the Managing Partner of Ridge with over 30 years of experience in the industry. With a proven track record of success and a deep understanding of the real estate market, Srinivas has established himself as a leading figure in the industry. He has dedicated his career to helping clients navigate the complex world of real estate, providing expert guidance and advice along the way. Srinivas is known for commitment to delivering results, earning him a reputation as one of the most reliable and trustworthy professionals in the business. Whether you are a first-time homebuyer or an experienced investor, Srinivas and his team have the expertise and experience to help you achieve your goals.",
+    description:
+      "Srinivas is the Managing Partner of Ridge with over 30 years of experience in the industry. With a proven track record of success and a deep understanding of the real estate market, Srinivas has established himself as a leading figure in the industry. He has dedicated his career to helping clients navigate the complex world of real estate, providing expert guidance and advice along the way. Srinivas is known for commitment to delivering results, earning him a reputation as one of the most reliable and trustworthy professionals in the business. Whether you are a first-time homebuyer or an experienced investor, Srinivas and his team have the expertise and experience to help you achieve your goals.",
     facebook: "#",
     linkedin: "#",
-    twitter: "#"
+    twitter: "#",
   },
   {
     image: amar,
     name: "Ramesh Amarnath V",
     title: "VP OF OPERATIONS",
-    description: "Mr. Ramesh Amarnath is a B.Tech graduate in Electrical and Electronics Engineering from JNTU Hyderabad, complemented by an MBA in HR. With over 25 years of extensive experience in various domains, including Construction Management, Infrastructure design, and implementation of large civil projects such as airports and 50+ storey structures, as well as social projects, audit, and compliance.He has held leadership roles in prominent MNCs such as A&P, HSBC EDPI, JLL, UBS India Service Centre, SKS Microfinance Ltd and others. He is a certified Professional Project Management (PPM) and Lean Management expert. He also holds qualifications in the Civil Infrastructure domain. Notably, he has served as the CEO at Community Pure Water (CPW), a non-profit ensuring rural clean water access, and at SKS Trusts, a Multi Crore+ entity dedicated to rural empowerment.",
+    description:
+      "Mr. Ramesh Amarnath is a B.Tech graduate in Electrical and Electronics Engineering from JNTU Hyderabad, complemented by an MBA in HR. With over 25 years of extensive experience in various domains, including Construction Management, Infrastructure design, and implementation of large civil projects such as airports and 50+ storey structures, as well as social projects, audit, and compliance.He has held leadership roles in prominent MNCs such as A&P, HSBC EDPI, JLL, UBS India Service Centre, SKS Microfinance Ltd and others. He is a certified Professional Project Management (PPM) and Lean Management expert. He also holds qualifications in the Civil Infrastructure domain. Notably, he has served as the CEO at Community Pure Water (CPW), a non-profit ensuring rural clean water access, and at SKS Trusts, a Multi Crore+ entity dedicated to rural empowerment.",
     facebook: "#",
     linkedin: "#",
-    twitter: "#"
+    twitter: "#",
   },
   {
     image: raghu,
-    name: "Ramesh Amarnath V",
-    title: "VP OF OPERATIONS",
-    description: "Mr. Ramesh Amarnath is a B.Tech graduate in Electrical and Electronics Engineering from JNTU Hyderabad, complemented by an MBA in HR. With over 25 years of extensive experience in various domains, including Construction Management, Infrastructure design, and implementation of large civil projects such as airports and 50+ storey structures, as well as social projects, audit, and compliance.He has held leadership roles in prominent MNCs such as A&P, HSBC EDPI, JLL, UBS India Service Centre, SKS Microfinance Ltd and others. He is a certified Professional Project Management (PPM) and Lean Management expert. He also holds qualifications in the Civil Infrastructure domain. Notably, he has served as the CEO at Community Pure Water (CPW), a non-profit ensuring rural clean water access, and at SKS Trusts, a Multi Crore+ entity dedicated to rural empowerment.",
+    name: "Raghu Raj Kumar Balaraju",
+    title: "PROJECT DIRECTOR & ADVISOR",
+    description:
+      "Raghu is a highly experienced Partner and Project Director in the real estate and hospitality industry, with over 40 years of experience. He has a wealth of knowledge and expertise in managing and executing large-scale real estate projects and hospitality ventures. Raghu’s leadership skills and strategic thinking have been instrumental in successfully delivering many high-profile projects at Ridge. He has a keen eye for identifying new business opportunities and has a track record of creating innovative solutions that drive business growth.",
     facebook: "#",
     linkedin: "#",
-    twitter: "#"
+    twitter: "#",
   },
   {
     image: kalyan,
-    name: "Ramesh Amarnath V",
-    title: "VP OF OPERATIONS",
-    description: "Mr. Ramesh Amarnath is a B.Tech graduate in Electrical and Electronics Engineering from JNTU Hyderabad, complemented by an MBA in HR. With over 25 years of extensive experience in various domains, including Construction Management, Infrastructure design, and implementation of large civil projects such as airports and 50+ storey structures, as well as social projects, audit, and compliance.He has held leadership roles in prominent MNCs such as A&P, HSBC EDPI, JLL, UBS India Service Centre, SKS Microfinance Ltd and others. He is a certified Professional Project Management (PPM) and Lean Management expert. He also holds qualifications in the Civil Infrastructure domain. Notably, he has served as the CEO at Community Pure Water (CPW), a non-profit ensuring rural clean water access, and at SKS Trusts, a Multi Crore+ entity dedicated to rural empowerment.",
+    name: "M Kalyan chakaravathy",
+    title: "AGM SALES & OPERATIONS",
+    description:
+      "Experienced AGM in Sales & Operations with 9 years of proven expertise. Proficient in driving revenue growth, optimizing operational efficiency, and leading high-performing teams. Adept at strategic planning, client relationship management, and achieving targets in dynamic markets. Demonstrated ability to streamline processes, enhance customer satisfaction, and deliver results in competitive industries.",
     facebook: "#",
     linkedin: "#",
-    twitter: "#"
+    twitter: "#",
   },
   {
     image: hema,
-    name: "Ramesh Amarnath V",
-    title: "VP OF OPERATIONS",
-    description: "Mr. Ramesh Amarnath is a B.Tech graduate in Electrical and Electronics Engineering from JNTU Hyderabad, complemented by an MBA in HR. With over 25 years of extensive experience in various domains, including Construction Management, Infrastructure design, and implementation of large civil projects such as airports and 50+ storey structures, as well as social projects, audit, and compliance.He has held leadership roles in prominent MNCs such as A&P, HSBC EDPI, JLL, UBS India Service Centre, SKS Microfinance Ltd and others. He is a certified Professional Project Management (PPM) and Lean Management expert. He also holds qualifications in the Civil Infrastructure domain. Notably, he has served as the CEO at Community Pure Water (CPW), a non-profit ensuring rural clean water access, and at SKS Trusts, a Multi Crore+ entity dedicated to rural empowerment.",
+    name: "P Hema",
+    title: "AGM FINANCE",
+    description:
+      "Accomplished AGM in Finance with a decade of extensive experience. Demonstrated proficiency in financial management, budgeting, and strategic planning. Proven track record of optimizing financial performance, reducing costs, and maximizing profitability. Skilled in team leadership, regulatory compliance, and risk management. Adept at leveraging financial insights to drive informed business decisions and achieve organizational objectives.",
     facebook: "#",
     linkedin: "#",
-    twitter: "#"
+    twitter: "#",
   },
-  
 ];
 
 const AboutUs = () => {
+  const [expanded, setExpanded] = useState(false);
+
+  const toggleDescription = () => {
+    setExpanded(!expanded);
+  };
+
   return (
     <>
       <Helmet>
@@ -93,14 +105,14 @@ const AboutUs = () => {
         </div>
         <div className="content">
           <h1 style={{ fontWeight: "bolder" }}>About Us</h1>
-          <p style={{ display: "flex" }}>
+          <p style={{ display: "flex", textAlign: "justify" }}>
             <span style={{ fontSize: "48px", fontWeight: "600" }}>|</span>
             <span style={{ padding: "12px", fontWeight: "bolder" }}>
               Ridge is a culmination of people with an acquired passion for
               conscious living. We believe learning and growth begins at home.
             </span>
           </p>
-          <p>
+          <p style={{ textAlign: "justify" }}>
             A home that is designed to uphold traditions and age-old practices
             is one that will stand the test of time. A recurring theme at Ridge
             is the creation of ideal living environments that honour the
@@ -132,20 +144,43 @@ const AboutUs = () => {
       <div className="teamSection">
         <h1>Meet Our Team</h1>
         <div className="teamMembers">
-          {teamMembers.map((member, index) => (
+          {/* {teamMembers.map((member, index) => (
             <div className="teamMember" key={index}>
               <img src={member.image} alt={member.name} className="teamImage" />
               <div className="teamDetails">
-                <h3 style={{fontFamily: "sans-serif", fontSize: '35px', fontWeight: '900', margin: "25px 0px"}}>{member.name}</h3>
+                <h3
+                  style={{
+                    fontFamily: "sans-serif",
+                    fontSize: "35px",
+                    fontWeight: "900",
+                    margin: "25px 0px",
+                  }}
+                >
+                  {member.name}
+                </h3>
                 <h4>{member.title}</h4>
-                <p>{member.description}</p>
+                <div className={`description ${expanded ? "expanded" : ""}`}>
+                  <p style={{ textAlign: "justify" }}>{member.description}</p>
+                </div>
+                <button onClick={toggleDescription} style={{color: '#DD9C37', border: 'none', fontSize: '20px', padding: '10px' }}>
+                  {expanded ? "...Read Less" : "...Read More"}
+                </button>
                 <div className="socialLinks">
-                  <a href={member.facebook}><i className="fa fa-facebook"></i></a>
-                  <a href={member.linkedin}><i className="fa fa-linkedin"></i></a>
-                  <a href={member.twitter}><i className="fa-brands fa-x-twitter"></i></a>
+                  <a href={member.facebook}>
+                    <i className="fa fa-facebook"></i>
+                  </a>
+                  <a href={member.linkedin}>
+                    <i className="fa fa-linkedin"></i>
+                  </a>
+                  <a href={member.twitter}>
+                    <i className="fa-brands fa-x-twitter"></i>
+                  </a>
                 </div>
               </div>
             </div>
+          ))} */}
+          {teamMembers.map((member, index) => (
+            <TeamMemberCard key={index} member={member} />
           ))}
         </div>
       </div>
